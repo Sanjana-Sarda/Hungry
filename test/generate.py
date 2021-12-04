15f13b7a3a47f5f7858e318d7e7050b7990ad569
Sanjana Sarda <31695528+Sanjana-Sarda@users.noreply.github.com>
2021-12-04 15:24:58 -0800

    retrain testing

--- test/generate.py
+++ test/generate.py
@@ -6,26 +6,32 @@ import sys
 import time
 
 
-def policyit(b, item, num, use):
-    policy = pd.read_csv("../train/"+item+".policy", header=None).to_numpy()
-    
-    state = 1
+def policyit(b, item, num, use, state):
+    #policy = pd.read_csv("../train/"+item+".policy", header=None).to_numpy()
+    policy = pd.read_csv("../retrain/"+item+".policy", header=None).to_numpy()
+    #state = 1
     pol = []
     for a in range (int(num)):
-        pol.append(int(policy[state-1][0]))
-        state = state + policy[state-1][0] - use[a][b]
+        p = int(policy[state-1][0])
+        state = state + p - use[a][b]
+        if (state==0):
+            p = p+1
+            state = state+1
+        pol.append(p)
+        
     return pol
 
 def generate (litems, num): 
-    use = pd.read_csv("use.csv", header=None).to_numpy()
+    use = pd.read_csv("ret_use.csv", header=None).to_numpy()
     use = use[0].reshape(int(num), len(litems))
     actions = np.zeros((int(num), len(litems)))
+    states = [3, 2, 2, 2, 2]
     for a in range (len(litems)):
-        actions[:,a] =  policyit(a, litems[a], num, use)
+        actions[:,a] =  policyit(a, litems[a], num, use, states[a])
     write_policy(actions.flatten())
     
 def write_policy(action):
-    with open("automated", 'w') as f:
+    with open("ret_automated", 'w') as f:
         for a in action:
             f.write("{}\n".format(int(a)))
 
