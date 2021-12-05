e8767a0cc4631182ef9bf2c255db443d9bf29046
Sanjana Sarda <31695528+Sanjana-Sarda@users.noreply.github.com>
2021-12-05 14:05:41 -0800

    fix

--- train/qlearn.py
+++ train/qlearn.py
@@ -6,11 +6,9 @@ import sys
 import time
 
 
-num_of_observations = 50000
-max_steps = 100
 alphas = np.linspace(1.0, 0.2, 100)
 alpha = 1.0
-gamma = 1.0
+gamma = 0.95
 eps = 0.2
 q_table = dict()
 
@@ -31,20 +29,14 @@ def setup(D):
     global actions 
     actions = [1, 2, 3, 4, 5, 6, 7] #D['a'].unique()
     print(actions)
-    #for o in range (num_of_observations):
-        #state = start_state
         
     for epochs in range (100):    
         alpha = alphas[epochs]
         for o in range (D.shape[0]):
             state, action, reward, next_state = D.iloc[o]
         
-        #action = action(state)
-        #next_state, reward, done = act(state, action)
             q_value(state)[int(action)-1] = q_value(state, int(action)-1) + alpha*(reward+gamma*np.max(q_value(next_state)) - q_value(state, int(action)-1))
-        #state = next_state
-        #if done:
-            #break
+      
 
 def make_policy(n):
     act = []
@@ -64,16 +56,8 @@ def compute(infile, n, outfile):
     start = time.time()
     D = pd.read_csv(infile) #dataset   
     setup(D)
-    #if(infile=="milk.csv"):
-     #   n = 4
-    #elif(infile=="orange1.csv"):
-     #   n = 5
-    #elif(infile=="bread.csv"):
-     #   n = 3
     action = make_policy(int(n))
     write_policy(action, outfile)
-    #vars = D.nunique(axis=0)
-    #print(vars)
     print (time.time()-start)
 
 def main():
