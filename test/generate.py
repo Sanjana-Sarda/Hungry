f97dd2bc7be7c86c5c48ec4da5c2c95902c0a687
Sanjana Sarda <31695528+Sanjana-Sarda@users.noreply.github.com>
2021-12-05 23:02:15 -0800

    fix again

--- test/generate.py
+++ test/generate.py
@@ -6,34 +6,41 @@ import sys
 import time
 
 
-def policyit(b, item, num, use, state):
+def policyit(b, item, num, total, state):
     #policy = pd.read_csv("../train/"+item+".policy", header=None).to_numpy()
     policy = pd.read_csv("../retrain/"+item+".policy", header=None).to_numpy()
     #state = 1
     pol = []
+    states = []
+    print (item)
     for a in range (int(num)):
-        p = int(policy[state-1][0])
-        state = state + p - use[a][b]
-        if (state==0):
-            p = p+1
-            state = state+1
+        p = int(policy[int(state)-1][0])
+        e = max(state -1 - total[a][b], 0) 
+        state = max(state + p - total[a][b] - e -1, 1)
         pol.append(p)
+        states.append(state)
         
-    return pol
+    return pol, states
 
 def generate (litems, num): 
-    use = pd.read_csv("ret_use.csv", header=None).to_numpy()
-    use = use[0].reshape(int(num), len(litems))
+    total = pd.read_csv("ret_use.csv", header=None).to_numpy()
+    #total = pd.read_csv("total.csv", header=None).to_numpy()
+    total = total[0].reshape(int(num), len(litems))
     actions = np.zeros((int(num), len(litems)))
+    exp = np.zeros((int(num), len(litems)))
     states = [3, 2, 2, 2, 2]
     for a in range (len(litems)):
-        actions[:,a] =  policyit(a, litems[a], num, use, states[a])
-    write_policy(actions.flatten())
+        actions[:,a], exp[:,a] =  policyit(a, litems[a], num, total, states[a])
+    write_policy(actions.flatten(), exp.flatten())
     
-def write_policy(action):
+def write_policy(action, exp):
     with open("ret_automated", 'w') as f:
+    #with open("automated", 'w') as f:
         for a in action:
             f.write("{}\n".format(int(a)))
+    with open("exp", 'w') as f:
+        for e in exp:
+            f.write("{}\n".format(int(e)))
 
 def compute(items, num):
     start = time.time()
